b7b405b94fca36d18e59b12cc319dc1e7bf19328
yltrcc <ttxxly@gmail.com>
2022-01-15 18:18:02 +0800

    新增API接口 - 随机生成指定数量的面试题-page版本

--- src/main/java/com/yltrcc/blog/api/ApiController.java
+++ src/main/java/com/yltrcc/blog/api/ApiController.java
@@ -49,6 +49,31 @@ public class ApiController {
     }
 
     /**
+     * 获取Html格式的面试题
+     * @return
+     */
+    @GetMapping("/random/getpage")
+    public String getpage(@RequestParam("nums") Integer nums) {
+
+        int count = questionsService.getCounts();
+
+        if (count == 0) {
+            return "Sorry, 没有数据，请添加数据";
+        }
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i=0; i<nums; i++) {
+            //获取随机数
+            int i2 = random.nextInt(count) + 1;
+            Question question = questionsService.findByQuestionsId(i2);
+            sb.append(question.getArticleContent());
+            sb.append("\n\n");
+        }
+
+        return sb.toString();
+    }
+
+    /**
      * 获取Json格式的面试题 用于安卓端口
      * @return
      */
