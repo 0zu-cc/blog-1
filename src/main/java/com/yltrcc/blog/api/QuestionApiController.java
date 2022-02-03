ab3b7cb2fe80edb435bf04465560fe11c943f4fc
yltrcc <ttxxly@gmail.com>
2022-02-03 14:14:50 +0800

    新增API接口 - 获取总的题库数量

---
+++ src/main/java/com/yltrcc/blog/api/QuestionApiController.java
@@ -0,0 +1,64 @@
+package com.yltrcc.blog.api;
+
+import com.yltrcc.blog.model.domain.Question;
+import com.yltrcc.blog.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+/**
+ * Package: com.yltrcc.blog.api
+ * Date：2022-02-03
+ * Time：13:21
+ * Description：用户获取面试题相关API
+ *
+ * @author yltrcc
+ * @version 1.0
+ */
+@RestController
+@RequestMapping(value = "/api/question")
+public class QuestionApiController {
+
+    /**
+     * 当前数据库中面试题的总数
+     */
+    int count = -1;
+
+    @Autowired
+    private QuestionService questionService;
+
+
+    @GetMapping("/getpage")
+    public String getpage(@RequestParam("id") int id) {
+
+        if (count == -1) {
+            count = questionService.getCounts();
+        }
+        if (count == 0) {
+            return "Sorry, 没有数据，请添加数据";
+        }
+        if (id >= count) {
+            return "Sorry, 当前数据库中没有此数据";
+        }
+        StringBuilder sb = new StringBuilder();
+        Question question = questionService.findByQuestionId(id);
+        sb.append(question.getArticleContent());
+        sb.append("\n\n");
+
+        return sb.toString();
+    }
+
+    @GetMapping("/getcount")
+    public int getcount() {
+
+        if (count == -1) {
+            count = questionService.getCounts();
+        }
+
+        return count;
+    }
+}
---
+++ src/main/java/com/yltrcc/blog/api/entity/ApiResponse.java
@@ -0,0 +1,81 @@
+package com.yltrcc.blog.api.entity;
+
+import java.util.List;
+
+/**
+ * Package: com.yltrcc.blog.api
+ * Date：2022-01-15
+ * Time：16:18
+ * Description：TODO
+ *
+ * @author yltrcc
+ * @version 1.0
+ */
+public class ApiResponse {
+
+    /**
+     * 请求结果HTTP状态码 200 表示成功
+     */
+    private int status;
+
+    /**
+     * 请求结果自定义状态码 表示具体的自定义错误码
+     * 错误码是40483，其中404代表了请求的资源不存在，而83则制定了这次出错，具体是哪一种资源不存在。
+     */
+    private int code;
+
+    /**
+     * 用户可理解的错误信息
+     */
+    private String message;
+
+    /**
+     * 该出错的详细技术信息，提供给客户端的开发者阅读
+     */
+    private String developerMessage;
+
+    /**
+     * 请求结果数据
+     */
+    private List<Object> data;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
+
+    public List<Object> getData() {
+        return data;
+    }
+
+    public void setData(List<Object> data) {
+        this.data = data;
+    }
+}
